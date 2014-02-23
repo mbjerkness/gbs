d964c532c20692a545cfbe1126d699614c891185
Mike Bjerkness <mike.bjerkness@gmail.com>
2014-02-23 16:36:13 -0600

    Adding more..

--- chef-repo/cookbooks/gbs/recipes/default.rb
+++ chef-repo/cookbooks/gbs/recipes/default.rb
@@ -8,9 +8,13 @@
 #
 
 include_recipe "apache2"
-include_recipe "nginx"
+#include_recipe "nginx"
 include_recipe "mysql::client"
 include_recipe "mysql::server"
 include_recipe "php"
 include_recipe "php::module_mysql"
 include_recipe "apache2::mod_php5"
+
+apache_site "default" do
+  enable true
+end
