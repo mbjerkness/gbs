038686b82d46f7dc931c14d01895c3cee920e282
mbjerkness <mbjerkness@8thbridge.com>
2014-02-25 09:59:47 -0600

    Updates

--- chef-repo/cookbooks/gbs/metadata.rb
+++ chef-repo/cookbooks/gbs/metadata.rb
@@ -6,7 +6,9 @@ description      'Installs/Configures gbs'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends "apache2"
+#depends "apache2"
 #depends "nginx"
-depends "mysql"
-depends "php"
+#depends "mysql"
+#depends "php"
+
+depends "wordpress-nginx"
--- chef-repo/cookbooks/gbs/recipes/default.rb
+++ chef-repo/cookbooks/gbs/recipes/default.rb
@@ -7,14 +7,31 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "apache2"
-#include_recipe "nginx"
-include_recipe "mysql::client"
-include_recipe "mysql::server"
-include_recipe "php"
-include_recipe "php::module_mysql"
-include_recipe "apache2::mod_php5"
 
-apache_site "default" do
-  enable true
+
+
+
+
+
+
+#include_recipe "wordpress-nginx"
+
+
+
+
+#include_recipe "apache2"
+include_recipe "nginx"
+#include_recipe "mysql::client"
+#include_recipe "mysql::server"
+#include_recipe "php"
+#include_recipe "php::module_mysql"
+#include_recipe "apache2::mod_php5"
+
+#apache_site "default" do
+#  enable true
+#end
+
+wordpress_nginx_site 'gracebiblestudies.com' do
+  host 'gracebiblestudies.com'
+  root '/srv/www/gracebiblestudies.com'
 end
